a1c4e543a0c197cf139968ec1738868d851fca24
Mark Armendariz <src@enobrev.com>
2012-09-19 14:10:44 +0000

    catching recursive json during send and using util.inspect to make sure it still spits out

--- test/test.js
+++ test/test.js
@@ -8,3 +8,16 @@
     syslog.alert('Alert');
     syslog.critical('Critical');
     syslog.emergency('Emergency');
+
+    syslog.error({action: 'error', error: new Error('This is an Error')});
+
+    var oRecursive = {
+        test: 1
+    };
+
+    oRecursive.recurse = oRecursive;
+    oRecursive.recurse2 = {
+        error: new Error('whatever'),
+        and: oRecursive
+    };
+    syslog.error({action: 'recursive', error: oRecursive});
